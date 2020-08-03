d0bbfbecb0db2d1524f71eb7a4f15030ffc20176
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-08-03 23:43:05 +0200

    fix search result bold text to work with tier 2

--- src/components/SuggestedCountries.js
+++ src/components/SuggestedCountries.js
@@ -1,21 +1,44 @@
 import React from 'react';
 
-function SuggestedCountries(props) {
-  return (
-    <div
-      id="suggestions-container"
-      className={props.suggestions.length === 0 ? 'hidden' : props.classes}
-    >
-      {props.suggestions.slice(0, 4).map((item) => (
-        <div className="flex-row justify-between" key={item}>
-          <p>{item}</p>
-          <button onClick={() => props.add(item)}>
-            <i className="fas fa-plus"></i>
-          </button>
-        </div>
-      ))}
-    </div>
-  );
+class SuggestedCountries extends React.Component {
+  constructor(props) {
+    super(props);
+    this.boldReplace = this.boldReplace.bind(this);
+  }
+
+  boldReplace(userString, suggestion) {
+    const marked = suggestion.replace(
+      new RegExp(userString, 'i'),
+      '@' + userString + '@'
+    );
+    return marked.split('@').map((element) => {
+      if (element === userString) {
+        return <strong>{suggestion.match(new RegExp(userString, 'i'))}</strong>;
+      } else {
+        return element;
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div
+        id="suggestions-container"
+        className={
+          this.props.suggestions.length === 0 ? 'hidden' : this.props.classes
+        }
+      >
+        {this.props.suggestions.slice(0, 4).map((item) => (
+          <div className="flex-row justify-between" key={item}>
+            <p>{this.boldReplace(this.props.input, item)}</p>
+            <button onClick={() => this.props.add(item)}>
+              <i className="fas fa-plus"></i>
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default SuggestedCountries;
