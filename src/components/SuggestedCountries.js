c29ff7f299ab41695b964f471782251c7045387b
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-08-03 18:00:06 +0200

    add plus icon

--- src/components/SuggestedCountries.js
+++ src/components/SuggestedCountries.js
@@ -9,7 +9,7 @@ function SuggestedCountries(props) {
       {props.suggestions.slice(0, 4).map((item) => (
         <div className="flex-row justify-between" key={item}>
           <p>{item}</p>
-          <button onClick={() => props.add(item)}>Add</button>
+          <button onClick={() => props.add(item)}><i className="fas fa-plus"></i></button>
         </div>
       ))}
     </div>
