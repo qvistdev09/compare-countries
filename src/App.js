b43d1a893451b2e507b118cb6a81c72c6147f03b
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-08-03 12:10:45 +0200

    make main component stateful

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './css/utilities.css';
 import './css/style.css';
 
-
-function App() {
-  return (
-    <div>
-      <p>Heya</p>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Heya</p>
+      </div>
+    );
+  }
 }
 
 export default App;
