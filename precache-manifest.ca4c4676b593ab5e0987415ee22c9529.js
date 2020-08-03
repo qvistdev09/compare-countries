8e8ec59faba3c60f13d703c8a2ec5d29a4ae6930
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-08-03 17:51:35 +0200

    Updates

---
+++ precache-manifest.ca4c4676b593ab5e0987415ee22c9529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98eac2c6bc3c8bb9a4e19c74ceb6cb2",
+    "url": "/compare-countries/index.html"
+  },
+  {
+    "revision": "7177aa2d1d3fdca144c5",
+    "url": "/compare-countries/static/css/main.431a5cc6.chunk.css"
+  },
+  {
+    "revision": "83f1196ce84b142c5163",
+    "url": "/compare-countries/static/js/2.68ed3340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/compare-countries/static/js/2.68ed3340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7177aa2d1d3fdca144c5",
+    "url": "/compare-countries/static/js/main.2720da75.chunk.js"
+  },
+  {
+    "revision": "f140a17dd3488ef97844",
+    "url": "/compare-countries/static/js/runtime-main.e0d6f125.js"
+  }
+]);
