ab12a7072bcb1c7a4f3a1f59065336140ed0f95b
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-08-03 17:46:27 +0200

    font change

--- src/components/Country.js
+++ src/components/Country.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Country(props) {
   return (
-    <div className="country-container">
-      <p>{props.name}</p>
-      <p>{props.population}</p>
-      <p>{props.capital}</p>
+    <div className="country-container m-bottom p-small">
+      <p>Name: {props.name}</p>
+      <p>Population: {props.population}</p>
+      <p>Capital: {props.capital}</p>
       <img src={props.flag} alt="flag" />
       <button onClick={() => props.delete(props.alpha2Code)}>Delete</button>
     </div>
