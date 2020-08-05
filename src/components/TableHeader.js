03acb04d6312500c7cbeac9fda7f85edf2febe04
Qvistsson <63651194+qvistsson@users.noreply.github.com>
2020-08-05 18:52:26 +0200

    style sort buttons

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -3,50 +3,50 @@ import React from 'react';
 function TableHeader(props) {
   return [
     <div key="header-cell-name" className="grid-cell header-end-left">
-      <p>Name</p>
+      <p className="m-right-small">Name</p>
       <i
         onClick={() => props.action('name', false)}
-        className="fas fa-caret-up"
+        className="fas fa-chevron-up sort-icon"
       ></i>
       <i
         onClick={() => props.action('name', true)}
-        className="fas fa-caret-down"
+        className="fas fa-chevron-down sort-icon"
       ></i>
     </div>,
     <div key="header-cell-flag" className="grid-cell header-middle">
       <p>Flag</p>
     </div>,
     <div key="header-cell-capital" className="grid-cell header-middle">
-      <p>Capital</p>
+      <p className="m-right-small">Capital</p>
       <i
         onClick={() => props.action('capital', false)}
-        className="fas fa-caret-up"
+        className="fas fa-chevron-up sort-icon"
       ></i>
       <i
         onClick={() => props.action('capital', true)}
-        className="fas fa-caret-down"
+        className="fas fa-chevron-down sort-icon"
       ></i>
     </div>,
     <div key="header-cell-population" className="grid-cell header-middle">
-      <p>Population</p>
+      <p className="m-right-small">Population</p>
       <i
         onClick={() => props.action('population', false)}
-        className="fas fa-caret-up"
+        className="fas fa-chevron-up sort-icon"
       ></i>
       <i
         onClick={() => props.action('population', true)}
-        className="fas fa-caret-down"
+        className="fas fa-chevron-down sort-icon"
       ></i>
     </div>,
     <div key="header-cell-area" className="grid-cell header-middle">
-      <p>Area</p>
+      <p className="m-right-small">Area</p>
       <i
         onClick={() => props.action('area', false)}
-        className="fas fa-caret-up"
+        className="fas fa-chevron-up sort-icon"
       ></i>
       <i
         onClick={() => props.action('area', true)}
-        className="fas fa-caret-down"
+        className="fas fa-chevron-down sort-icon"
       ></i>
     </div>,
     <div key="header-cell-delete" className="grid-cell header-end-right"></div>,
