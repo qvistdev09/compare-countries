b554ffbc2798ffe0bb315f461f55aaef98e052a4
Oscar Lindqvist <olindqv@gmail.com>
2021-12-26 08:14:48 -0800

    refactor SiteTitle

--- src/components/SiteTitle.js
+++ src/components/SiteTitle.js
@@ -1,12 +1,10 @@
-import React from 'react';
-
-function SiteTitle(props) {
+const SiteTitle = () => {
   return (
-    <div className={props.classes}>
+    <div className="flex-row align-center justify-between m-bottom-small screen-small-no-m-bottom screen-small-m-left">
       <i className="fas h1-icon fa-globe-africa screen-small-m-right"></i>
       <h1 className="screen-small-m-right">COMPARE COUNTRIES</h1>
     </div>
   );
-}
+};
 
 export default SiteTitle;
