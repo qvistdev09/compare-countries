cd4584316945c55c058f55f01ea2c195f017307f
Oscar Lindqvist <olindqv@gmail.com>
2021-12-26 13:38:20 -0800

    fix useeffect refs

--- src/hooks/useMediaQuery.js
+++ src/hooks/useMediaQuery.js
@@ -2,16 +2,17 @@ import { useEffect, useRef, useState } from "react";
 
 const useMediaQuery = (query) => {
   const [matches, setMatches] = useState(false);
-  const mediaQueryList = useRef(window.matchMedia(query));
+  const mediaQueryList = useRef();
 
   useEffect(() => {
+    mediaQueryList.current = window.matchMedia(query);
     const listener = (e) => setMatches(e.matches);
     mediaQueryList.current.addEventListener("change", listener);
 
     return () => {
       mediaQueryList.current.removeEventListener("change", listener);
     };
-  }, []);
+  }, [query]);
 
   return matches;
 };
