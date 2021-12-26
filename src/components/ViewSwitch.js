5c7a0cae33ffe08c2c55ad72c415dc86157b8077
Oscar Lindqvist <olindqv@gmail.com>
2021-12-26 08:14:55 -0800

    refactor view mode switch

--- src/components/ViewSwitch.js
+++ src/components/ViewSwitch.js
@@ -1,31 +1,27 @@
-import React from 'react';
-
-function ViewSwitch(props) {
+const ViewSwitch = ({ toggleView, listView }) => {
   return (
     <div
-      className={
-        props.classes + ' view-switch flex-row justify-between align-stretch'
-      }
-      onClick={props.toggleView}
+      className="screen-small-m-right view-switch flex-row justify-between align-stretch"
+      onClick={toggleView}
     >
       <div className="switch-icon-wrapper switch-icon-wrapper-active">
         <i
           className={
-            'far fa-list-alt switch-icon' +
-            (props.listView === true ? ' switch-icon-active' : '')
+            "far fa-list-alt switch-icon" +
+            (listView === true ? " switch-icon-active" : "")
           }
         ></i>
       </div>
       <div className="switch-icon-wrapper">
         <i
           className={
-            'fas fa-chart-bar switch-icon' +
-            (props.listView === false ? ' switch-icon-active' : '')
+            "fas fa-chart-bar switch-icon" +
+            (listView === false ? " switch-icon-active" : "")
           }
         ></i>
       </div>
     </div>
   );
-}
+};
 
 export default ViewSwitch;
