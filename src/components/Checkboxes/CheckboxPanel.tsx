511874315bea1b378e28a4df39691defc5789843
Oscar Lindqvist <olindqv@gmail.com>
2022-09-08 13:39:22 -0700

    Create CheckboxPanel

---
+++ src/components/Checkboxes/CheckboxPanel.tsx
@@ -0,0 +1,29 @@
+export default function CheckboxPanel({ checkboxes }: Props) {
+  return (
+    <div className="hide screen-small-flex-row screen-small-align-center screen-small-m-left">
+      {checkboxes.map((checkbox) => (
+        <div
+          key={checkbox.label}
+          className={`flex-row align-center m-right${checkbox.enabled ? "" : " low-opacity"}`}
+        >
+          <i
+            className={`${
+              checkbox.checked ? "far fa-check-square" : "far fa-square"
+            } checkbox-icon`}
+            onClick={checkbox.enabled ? checkbox.onClick : undefined}
+          />
+          <p className="checkbox-label m-left-small">{checkbox.label}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+interface Props {
+  checkboxes: Array<{
+    enabled: boolean;
+    checked: boolean;
+    label: string;
+    onClick: () => void;
+  }>;
+}
