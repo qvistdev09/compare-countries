68359a275d60bb56a818e7e3e5a13c12a1d3ea75
Oscar Lindqvist <olindqv@gmail.com>
2022-09-11 11:31:39 -0700

    Create apptitle component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./styles/utilities.css";
 import "./styles/style.css";
+import AppTitle from "./components/AppTitle";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <AppTitle />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/AppTitle/AppTitle.tsx
@@ -0,0 +1,8 @@
+export default function AppTitle() {
+  return (
+    <div className="flex-row align-center justify-between m-bottom-small screen-small-no-m-bottom screen-small-m-left">
+      <i className="fas h1-icon fa-globe-africa screen-small-m-right"></i>
+      <h1 className="screen-small-m-right">COMPARE COUNTRIES</h1>
+    </div>
+  );
+}
