65bb552e911f7938461db340263e93d4813e2abe
Oscar Lindqvist <olindqv@gmail.com>
2022-09-14 13:29:44 -0700

    Create api method

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,8 +1,20 @@
-export function getCountryByCode(code: string) {
-  return fetch(`https://restcountries.com/v3.1/alpha/${code}`).then((response) => {
-    if (response.status > 399) {
-      throw response;
-    }
-    return response.json();
-  });
+import { Country } from "../hooks/useSelectCountries";
+
+export function getCountryByCode(code: string): Promise<Country> {
+  return fetch(`https://restcountries.com/v2/alpha/${code}`)
+    .then((response) => {
+      if (response.status > 399) {
+        throw response;
+      }
+      return response.json();
+    })
+    .then((data) => ({
+      alpha2Code: data.alpha2Code as string,
+      flag: data.flag as string,
+      capital: data.capital as string,
+      population: data.population as number,
+      area: data.area as number,
+      demonym: data.demonym as string,
+      gini: data.gini as number,
+    }));
 }
