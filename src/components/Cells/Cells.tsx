902c402485118b0981cd5630f3cbd2c834d996e2
Oscar Lindqvist <olindqv@gmail.com>
2022-10-06 06:47:22 -0700

    Add format number function

--- src/components/Cells/Cells.tsx
+++ src/components/Cells/Cells.tsx
@@ -18,6 +18,35 @@ function getClassName(isLastRow: boolean, isShaded: boolean, columnPosition: str
   return `${shadeStatus} ${position} grid-cell`;
 }
 
+function formatNumber(value: number | undefined, type: "population" | "area" | "gini") {
+  if (typeof value !== "number") {
+    return "n/a";
+  }
+  switch (type) {
+    case "population":
+      if (value > 1000000) {
+        const toMillion = value / 1000000;
+        return Math.round(toMillion * 10) / 10 + " mil";
+      } else if (value > 10000) {
+        const toThousand = value / 1000;
+        return Math.round(toThousand * 10) / 10 + " k";
+      } else {
+        return value;
+      }
+    case "area":
+      return [
+        value.toLocaleString() + " km",
+        <span key="raised-span" className="raised">
+          2
+        </span>,
+      ];
+    case "gini":
+      return value + "%";
+    default:
+      return value;
+  }
+}
+
 interface CellProps {
   country: Country;
   columnPosition: "left-end" | "middle" | "right-end";
