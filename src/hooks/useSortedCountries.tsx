62195d70cb583d2cf2fd57ccc9e62414124221a0
Oscar Lindqvist <olindqv@gmail.com>
2022-10-10 03:45:38 -0700

    Create sort hook

---
+++ src/hooks/useSortedCountries.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Country } from "../types";
+
+export default function useSortedCountries(
+  defaultSortProperty: string,
+  defaultSortDirection: "ASC" | "DESC",
+  selectedCountries: Country[]
+) {
+  const [currentProperty, setCurrentProperty] = useState(defaultSortProperty);
+  const [currentDirection, setCurrentDirection] = useState(defaultSortDirection);
+
+  const sortedCountries = [...selectedCountries].sort((countryA, countryB) => {
+    const valueA = (countryA as any)[currentProperty.toLowerCase()];
+    const valueB = (countryB as any)[currentProperty.toLowerCase()];
+    if (currentDirection === "ASC") {
+      return valueA < valueB ? 1 : -1;
+    }
+    return valueA < valueB ? -1 : 1;
+  });
+
+  return {
+    setSort: (property: string, direction: "ASC" | "DESC") => {
+      setCurrentProperty(property);
+      setCurrentDirection(direction);
+    },
+    currentSort: {
+      column: currentProperty,
+      direction: currentDirection,
+    },
+    sortedCountries,
+  };
+}
