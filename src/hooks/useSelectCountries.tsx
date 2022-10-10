5e469c1ee41e582dd749ae86a42fe08e7c4c8762
Oscar Lindqvist <olindqv@gmail.com>
2022-10-10 04:26:12 -0700

    Implement random countries on load

--- src/hooks/useSelectCountries.tsx
+++ src/hooks/useSelectCountries.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getCountryByCode } from "../api";
 import { Country } from "../types";
+import countriesMap from "../consts/countriesMap.json";
 
 const cache = new Map<string, Country>();
 
@@ -24,9 +25,42 @@ export default function useSelectCountries() {
     setSelectedCountries(selectedCountries.filter((country) => country.alpha2Code !== code));
   }
 
+  useEffect(() => {
+    Promise.all(
+      getRandomCountries(10)
+        .map((name) => (countriesMap as any)[name])
+        .map((code) => getCountryByCode(code))
+    )
+      .then((countries) => {
+        countries.forEach((country) => {
+          cache.set(country.alpha2Code, country);
+        });
+        setSelectedCountries([...selectedCountries, ...countries]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return {
     selectedCountries,
     removeCountryByCode,
     selectCountryByCode,
   };
+}
+
+function getRandomCountries(amount: number) {
+  const countriesKeys = Object.keys(countriesMap);
+  const countriesToAdd: string[] = [];
+  while (countriesToAdd.length !== amount) {
+    const randomKey = countriesKeys[randomInt(0, countriesKeys.length)];
+    if (!countriesToAdd.includes(randomKey)) {
+      countriesToAdd.push(randomKey);
+    }
+  }
+  return countriesToAdd;
+}
+
+function randomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
