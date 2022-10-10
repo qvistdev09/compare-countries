d01a336f137269deaf01122b5c75b1e0e5e4b63f
Oscar Lindqvist <olindqv@gmail.com>
2022-10-10 04:02:45 -0700

    Create media query hook

---
+++ src/hooks/useMediaQuery.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function useMediaQuery(query: string) {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query);
+    if (mediaQuery.matches) {
+      setMatches(true);
+    }
+    function callback(e: MediaQueryListEvent) {
+      setMatches(e.matches);
+    }
+    mediaQuery.addEventListener("change", callback);
+    return () => {
+      mediaQuery.removeEventListener("change", callback);
+    };
+  }, []);
+
+  return matches;
+}
