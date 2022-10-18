ddbb9b3785801c2371fadc6fd5c992b132e6c18b
Oscar Lindqvist <olindqv@gmail.com>
2022-10-18 06:00:17 -0700

    Create error modal component

---
+++ src/components/ErrorModal/ErrorModal.tsx
@@ -0,0 +1,23 @@
+export default function ErrorModal({ onCloseAction }: Props) {
+  return (
+    <div id="error-modal">
+      <div className="modal-header-div flex-row align-center p-small">
+        <i className="fas fa-exclamation-circle error-icon m-right-small"></i>
+        <p>Oh no!</p>
+      </div>
+      <div className="p">
+        <p className="error-description m-bottom">
+          It appears the REST Countries API is offline right now - you can still compare the cached countries, but new
+          ones can't be added. Please try again later!
+        </p>
+        <button className="maxed error-btn" onClick={onCloseAction}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
+
+interface Props {
+  onCloseAction: () => void;
+}
